593943c56ff33f5fa4659e8c131057d585ccdbd7
max314 <max314@mail.ru>
2015-01-12 17:03:01 +0300

    перенос задачи домой

--- app/src/main/java/ru/max314/cardashboard/model/LocationVerifyListiner.java
+++ app/src/main/java/ru/max314/cardashboard/model/LocationVerifyListiner.java
@@ -2,7 +2,12 @@ package ru.max314.cardashboard.model;
 
 import android.location.Location;
 
+import org.xml.sax.Locator;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import ru.max314.util.LocationUtils;
 import ru.max314.util.LogHelper;
@@ -13,36 +18,53 @@ import ru.max314.util.LogHelper;
  */
 public class LocationVerifyListiner {
     static LogHelper Log = new LogHelper(LocationVerifyListiner.class);
-    private transient Location lastKnowLocation;
-    private transient Location currentLocation;
+    private static final int MAX_DEEP = 200;
+    private String locationDescription = "";
+    private int locationDublicates = 0;
+
+    private transient List<Location> locationList = new ArrayList<Location>();
     private Date startDate = new Date();
+    private Date lastAdd = null;
 
 
     public synchronized void addFromLocation(Location location){
-        // первый запуск
-        if (lastKnowLocation==null){
-            lastKnowLocation=location;
-            return;
+
+        lastAdd = new Date();
+        locationList.add(location);
+        while (locationList.size()>MAX_DEEP){ // если перебрали глубину подчищаем первые элементы
+            locationList.remove(0);
         }
 
-        // если есь скорость но не меняться местоположение - то жопа
-        if (!LocationUtils.isSpeedZerro(location)){
-            if (!(location.getLatitude() == lastKnowLocation.getLatitude() && location.getLongitude()==lastKnowLocation.getLongitude())){
-                lastKnowLocation=currentLocation;
-            }
+        // сравниваем паттерны
+        String buff = createLocationDescription(location);
+        if (buff.equals(locationDescription)){
+            locationDublicates++;
         }else {
-            lastKnowLocation=currentLocation;
+            locationDublicates=0;
+            locationDescription = buff;
         }
-        currentLocation = location;
+    }
+
+    private String createLocationDescription(Location location){
+        String str = String.format("%20.17f;%20.17f;%10.5f",location.getLatitude(),location.getLongitude(),location.getSpeed() );
+        return str;
+    }
+
+    /**
+     * Если GPS залип количество повторяющихся местоположений
+     * @return
+     */
+    public int getLocationDublicates() {
+        return locationDublicates;
     }
 
     public synchronized String Verify(){
         String result = "";
         // были данные ?
-        if (lastKnowLocation==null){
-            result = "Данных от GPS не поступало";
+        Date now = new Date();
+        if (Math.abs(now.getTime() - lastAdd.getTime())> TimeUnit.MILLISECONDS.convert(5,TimeUnit.MINUTES) ){
+            result = "Данных от GPS не поступало 5 минут";
         }else{
-            // проверрим время последнего фикса
 
             Date nowDateLastKnow = new Date(lastKnowLocation.getTime());
             long nowLastKnow = nowDateLastKnow.getMinutes()*60+nowDateLastKnow.getSeconds();
