ea923653e58d4b77c6db14bebf52458d140c9a6a
max314 <max314@mail.ru>
2015-01-13 15:16:44 +0300

    Управление с кнопочек PowerAmp

--- app/src/main/java/ru/max314/cardashboard/view/MusicUNIFragment.java
+++ app/src/main/java/ru/max314/cardashboard/view/MusicUNIFragment.java
@@ -1,6 +1,7 @@
 package ru.max314.cardashboard.view;
 
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -8,8 +9,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.maxmpz.poweramp.player.PowerampAPI;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnItemLongClick;
+import butterknife.OnLongClick;
 import ru.max314.cardashboard.R;
 import ru.max314.music.UniversalMusicOperation;
 
@@ -35,17 +40,28 @@ public class MusicUNIFragment extends Fragment {
 
     @OnClick(R.id.btPause)
     public void pauseClick(View view) {
-        new UniversalMusicOperation(true).pause();
+        this.getActivity().startService(new Intent(PowerampAPI.ACTION_API_COMMAND).putExtra(PowerampAPI.COMMAND, PowerampAPI.Commands.TOGGLE_PLAY_PAUSE));
     }
     @OnClick(R.id.btNext)
     public void nextClick(View view) {
-        new UniversalMusicOperation(true).nextTrack();
+        this.getActivity().startService(new Intent(PowerampAPI.ACTION_API_COMMAND).putExtra(PowerampAPI.COMMAND, PowerampAPI.Commands.NEXT));
+    }
+
+    @OnLongClick(R.id.btNext)
+    public boolean nextLongClick(View view) {
+        this.getActivity().startService(new Intent(PowerampAPI.ACTION_API_COMMAND).putExtra(PowerampAPI.COMMAND, PowerampAPI.Commands.NEXT_IN_CAT));
+        return true;
     }
 
     @OnClick(R.id.btStart)
     public void startClick(View view) {
-        String a_package = "com.maxmpz.audioplayer";
-        Intent LaunchIntent = this.getActivity().getPackageManager().getLaunchIntentForPackage(a_package);
-        startActivity(LaunchIntent);
+        startActivity(
+                new Intent()
+                        .setComponent(new ComponentName(PowerampAPI.PACKAGE_NAME, PowerampAPI.ACTIVITY_PLAYER_UI))
+                        .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+//        startActivity(new Intent(PowerampAPI.ACTION_PLASHOW_CURRENT));
+//        String a_package = "com.maxmpz.audioplayer";
+//        Intent LaunchIntent = this.getActivity().getPackageManager().getLaunchIntentForPackage(a_package);
+//        startActivity(LaunchIntent);
     }
 }
